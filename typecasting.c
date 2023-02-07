6ef84f9be28e8f65b2adab1af175155a4680eaba
Prateek <Prateekrajgrd74@gmail.com>
2023-02-07 23:09:59 +0530

    typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=3;
+    float b=54;
+    float c= (float)44/5;
+    printf("The value of a is %d\n",a);
+    printf("The value of b is %d\n",(int)b);
+    printf("The value of c is %f\n",c);
+    return 0;
+}
