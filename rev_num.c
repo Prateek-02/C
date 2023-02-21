f97f9fd67b148428829f992ca3bb130e70f9ec9b
Prateek <Prateekrajgrd74@gmail.com>
2023-02-21 23:16:05 +0530

    rev_num.c

--- rev_num.c
+++ rev_num.c
@@ -4,10 +4,12 @@ int main(){
     printf("Enter the number: ");
     scanf("%d",&a);
     int temp = a;
-    while(a>0){
-        int d = a%10;
+    while (a>0)     
+    {
+        d = a%10;
         rev = rev*10 + d;
         a = (int)a/10;
     }
     printf("reverse of %d is %d",temp,rev);
+    
 }
