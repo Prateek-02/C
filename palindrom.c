a23d73e4e7e286849755a22f9b9599126521cb33
Prateek <Prateekrajgrd74@gmail.com>
2023-03-11 00:25:03 +0530

    palindrom.c

---
+++ palindrom.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int ispalindrom(int num)
+{
+    int rev = 0;
+    int temp = num;
+    while (num > 0)
+    {
+        int d = num % 10;
+        rev = rev * 10 + d;
+        num = (int)num / 10;
+    }
+
+    printf("The reversed number is: %d\n", rev);
+
+    if(temp==rev)
+    {
+        if(rev%temp==0){
+            printf("It is palindrom\n");
+        }
+      
+    }
+    else{
+        printf("It is not palindrom\n");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    ispalindrom(n);
+}
