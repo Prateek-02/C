0aed726c3c1dd7b128d083463364058cad72954d
Prateek <Prateekrajgrd74@gmail.com>
2023-04-23 10:04:15 +0530

    array_bubble_sort.c

--- array_bubble_sort.c
+++ array_bubble_sort.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+/*
 void printArray(int* A,int n){
     for(int i=0;i<n;i++){
         printf("%d ",A[i]);
@@ -29,4 +30,35 @@ int main(){
     bubblesort(A,n);       // Function to sort the array
     printArray(A,n);       // Printing the array after sorting
 
+}
+*/
+
+
+void printArray(int* A,int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+
+void bubblesort(int* A,int n){
+    int temp;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(A[j]>A[j+1]){
+                temp=A[j];
+                A[j]=A[j+1];
+                A[j+1]=temp;
+            }
+        }
+    }
+}
+int main(){
+    int A[]={12,45,6,78,9,67};
+    int n = 6;
+    printArray(A,n);
+    bubblesort(A,n);
+    printArray(A,n);
 }
