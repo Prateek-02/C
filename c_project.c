5935ee11f6d50246ec10505b6f097bf0f48dcda6
Prateek <Prateekrajgrd74@gmail.com>
2023-04-26 15:21:03 +0530

    c_project.c

--- c_project.c
+++ c_project.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-// structure for holding property information
-struct Property {
+// Structure for holding property information
+struct Property
+{
     int id;
     char address[100];
     char type[20];
     int price;
 };
 
-// function prototypes
+// function Prototypes
 void add_property();
 void delete_property();
 void update_property();
 void buy_property();
 void sell_property();
 
-int main() {
+int main()
+{
     int choice;
     printf("Welcome to Real Estate Advisor!\n");
     printf("1. Add Property\n");
@@ -27,37 +29,42 @@ int main() {
     printf("5. Sell Property\n");
     printf("6. Quit\n");
 
-    do {
+    do
+    {
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                add_property();
-                break;
-            case 2:
-                delete_property();
-                break;
-            case 3:
-                update_property();
-                break;
-            case 4:
-                buy_property();
-                break;
-            case 5:
-                sell_property();
-                break;
-            case 6:
-                printf("Thank you for using Real Estate Advisor!\n");
-                break;
-            default:
-                printf("Invalid choice, please try again.\n");
+        switch (choice)
+        {
+        case 1:
+            add_property();
+            break;
+        case 2:
+            delete_property();
+            break;
+        case 3:
+            update_property();
+            break;
+        case 4:
+            buy_property();
+            break;
+        case 5:
+            sell_property();
+            break;
+        case 6:
+            printf("Thank you for using Real Estate Advisor!\n");
+            break;
+        default:
+            printf("Invalid choice, please try again.\n");
         }
     } while (choice != 6);
 
     return 0;
 }
 
-void add_property() {
+// Adding Property
+
+void add_property()
+{
     FILE *fp;
     struct Property p;
     fp = fopen("properties.txt", "a");
@@ -73,7 +80,10 @@ void add_property() {
     fclose(fp);
 }
 
-void delete_property() {
+// Deleting Property
+
+void delete_property()
+{
     FILE *fp, *temp;
     int id;
     struct Property p;
@@ -81,8 +91,10 @@ void delete_property() {
     temp = fopen("temp.txt", "w");
     printf("Enter ID of property to delete: ");
     scanf("%d", &id);
-    while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF) {
-        if (p.id != id) {
+    while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF)
+    {
+        if (p.id != id)
+        {
             fprintf(temp, "%d %s %s %d\n", p.id, p.address, p.type, p.price);
         }
     }
@@ -92,7 +104,10 @@ void delete_property() {
     rename("temp.txt", "properties.txt");
 }
 
-void update_property() {
+// Updating Property
+
+void update_property()
+{
     FILE *fp, *temp;
     int id, found = 0;
     struct Property p;
@@ -100,8 +115,10 @@ void update_property() {
     temp = fopen("temp.txt", "w");
     printf("Enter ID of property to update: ");
     scanf("%d", &id);
-    while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF) {
-        if (p.id == id) {
+    while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF)
+    {
+        if (p.id == id)
+        {
             found = 1;
             printf("Enter new address: ");
             scanf("%s", p.address);
@@ -110,71 +127,90 @@ void update_property() {
             printf("Enter new price: ");
             scanf("%d", &p.price);
         }
-        fprintf(temp, "%d %s %s %s %d\n", p.id, p.address, p.type, p.price);
-}
-fclose(fp);
-fclose(temp);
-remove("properties.txt");
-rename("temp.txt", "properties.txt");
-if (!found) {
-printf("Property not found.\n");
-} else {
-printf("Property updated.\n");
-}
-}
-
-void buy_property() {
-FILE *fp, *temp;
-int id, found = 0;
-struct Property p;
-fp = fopen("properties.txt", "r");
-temp = fopen("temp.txt", "w");
-printf("Enter ID of property to buy: ");
-scanf("%d", &id);
-while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF) {
-if (p.id == id) {
-found = 1;
-printf("Property bought:\n");
-printf("Address: %s\n", p.address);
-printf("Type: %s\n", p.type);
-printf("Price: %d\n", p.price);
-} else {
-fprintf(temp, "%d %s %s %d\n", p.id, p.address, p.type, p.price);
-}
-}
-fclose(fp);
-fclose(temp);
-remove("properties.txt");
-rename("temp.txt", "properties.txt");
-if (!found) {
-printf("Property not found.\n");
-}
+        fprintf(temp, "%d %s %s %d\n", p.id, p.address, p.type, p.price);
+    }
+    fclose(fp);
+    fclose(temp);
+    remove("properties.txt");
+    rename("temp.txt", "properties.txt");
+    if (!found)
+    {
+        printf("Property not found.\n");
+    }
+    else
+    {
+        printf("Property updated.\n");
+    }
 }
 
-void sell_property() {
-FILE *fp, *temp;
-int id, found = 0;
-struct Property p;
-fp = fopen("properties.txt", "r");
-temp = fopen("temp.txt", "w");
-printf("Enter ID of property to sell: ");
-scanf("%d", &id);
-while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF) {
-if (p.id == id) {
-found = 1;
-printf("Property sold:\n");
-printf("Address: %s\n", p.address);
-printf("Type: %s\n", p.type);
-printf("Price: %d\n", p.price);
-} else {
-fprintf(temp, "%d %s %s %d\n", p.id, p.address, p.type, p.price);
+// Buying Property
+
+void buy_property()
+{
+    FILE *fp, *temp;
+    int id, found = 0;
+    struct Property p;
+    fp = fopen("properties.txt", "r");
+    temp = fopen("temp.txt", "w");
+    printf("Enter ID of property to buy: ");
+    scanf("%d", &id);
+    while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF)
+    {
+        if (p.id == id)
+        {
+            found = 1;
+            printf("Property bought:\n");
+            printf("Address: %s\n", p.address);
+            printf("Type: %s\n", p.type);
+            printf("Price: %d\n", p.price);
+        }
+        else
+        {
+            fprintf(temp, "%d %s %s %d\n", p.id, p.address, p.type, p.price);
+        }
+    }
+    fclose(fp);
+    fclose(temp);
+    remove("properties.txt");
+    rename("temp.txt", "properties.txt");
+    if (!found)
+    {
+        printf("Property not found.\n");
+    }
 }
-}
-fclose(fp);
-fclose(temp);
-remove("properties.txt");
-rename("temp.txt", "properties.txt");
-if (!found) {
-printf("Property not found.\n");
-}
+
+// Selling Property
+
+void sell_property()
+{
+    FILE *fp, *temp;
+    int id, found = 0;
+    struct Property p;
+    fp = fopen("properties.txt", "r");
+    temp = fopen("temp.txt", "w");
+    printf("Enter ID of property to sell: ");
+    scanf("%d", &id);
+    while (fscanf(fp, "%d %s %s %d", &p.id, p.address, p.type, &p.price) != EOF)
+    {
+        if (p.id == id)
+        {
+            found = 1;
+            printf("Property sold:\n");
+            printf("Address: %s\n", p.address);
+            printf("Type: %s\n", p.type);
+            printf("Price: %d\n", p.price);
+        }
+        else
+        {
+            fprintf(temp, "%d %s %s %d\n", p.id, p.address, p.type, p.price);
+        }
+    }
+    fclose(fp);
+    fclose(temp);
+    remove("properties.txt");
+    rename("temp.txt", "properties.txt");
+    if (!found)
+    {
+        printf("Property not found.\n");
+    }
 }
